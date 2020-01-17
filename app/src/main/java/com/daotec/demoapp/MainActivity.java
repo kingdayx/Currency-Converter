0c4ac8ab9fb7bd78fe140d00aaaacf8ad20fc68e
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-01-17 18:23:54 -0500

    Initial commit

---
+++ app/src/main/java/com/daotec/demoapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.daotec.demoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void clickFunction(View view) {
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
