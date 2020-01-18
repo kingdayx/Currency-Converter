23c5668a49655c102fc9dc1ade658a1985430ac3
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-01-17 21:16:56 -0500

    fixed contraints in UI and provided java backend
    fixed contraints in xml layout and provided java code for the backed of the app to convert the euro integer provided by the user into usd

--- app/src/main/java/com/daotec/demoapp/MainActivity.java
+++ app/src/main/java/com/daotec/demoapp/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public void clickFunction(View view) {
-
-
-    }
+    public void convert(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String amountInPounds = editText.getText().toString();
+        double amountInPoundsDouble = Double.parseDouble(amountInPounds);
+        double amountInDollarsDouble= amountInPoundsDouble * 1.3;
+        String amountInDollarsString = String.format("%.2f",amountInDollarsDouble);
+        Log.i("Amount in dollars", amountInDollarsString);
+        Toast.makeText(this, amountInPounds + " euros is " + amountInDollarsString + " USD", Toast.LENGTH_LONG).show();
+   }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
